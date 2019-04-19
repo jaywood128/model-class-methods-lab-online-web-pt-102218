cacf32fd2eef2e579e5c1402da8c1c023500e4e3
Johnathon Wood <jaywood128@gmail.com>
2019-04-19 17:16:45 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,31 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five 
+    Boat.all.limit(5).map do |b|
+      b
+    end
+   end
+
+   def self.dinghy 
+    @dinghies = []
+    Boat.all.each do |b|
+      if b.length <=20 
+        @dinghies << b 
+      else 
+
+      end
+    end
+    @dinghies 
+  end
+
+  def self.ship
+    Boat.where("length > '20'")
+  end 
+
+  def self.last_three_alphabetically
+    order(:name).last(3).reverse
+  end
+
 end
